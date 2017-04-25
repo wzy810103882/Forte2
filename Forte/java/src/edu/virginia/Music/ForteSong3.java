00f703babd668b7b0b84105e3bd520b904a7b9ff
Gannon Combs <gc8um@virginia.edu>
2017-04-24 22:23:52 -0400

    Music folder?

---
+++ Forte/java/src/edu/virginia/Music/ForteSong3.java
@@ -0,0 +1,87 @@
+package edu.virginia.Music;
+
+import jm.JMC;
+import jm.audio.Instrument;
+import jm.music.data.Note;
+import jm.music.rt.RTLine;
+
+import javax.swing.*;
+
+public class ForteSong3 extends RTLine implements JMC {
+    private Note n = new Note(36, 0.5);
+    private int dynoPosition = 0;
+
+    //i added these. currently not using.
+    //private int[] pitchArray = new int[] {C4,C4,C4,D4,E4,E4,D4,E4,F4,G4,C5,C5,C5,G4,G4,G4,E4,E4,E4,C4,C4,C4,G4,F4,E4,D4,C4};
+    //private double[] rhythmArray = new double[] {QN,QN,QNT,ENT,QN,QNT,ENT,QNT,QT,HN,
+    //      ENT,ENT,ENT,ENT,ENT,ENT,ENT,ENT,ENT,ENT,ENT,ENT,QNT,ENT,QNT,ENT,HN};
+    private int[] whistlePitchArray = new int[] {REST, REST, REST, REST, REST, D4, D4, REST, D4, REST};
+    private double[] whistleRhythmArray = new double[] {QN, QN, QN, QN, QN, EN, EN, QN, EN, EN };
+    int arrayIndex = 0;
+
+    /**
+     * Constructor
+     */
+    public ForteSong3(Instrument[] instArray) {
+        super(instArray);
+    }
+
+    /**
+     * Generate the next note when requested.
+     */
+    public synchronized Note getNextNote() {
+        /*n.setPitch(pitch + intervals[(int) (Math.random() * intervals.length)]);
+        //n.setDynamic((int) (Math.random() * 80 + 45));
+        n.setDynamic(dynoPosition);
+        n.setPan(panPosition);
+        n.setRhythmValue((int) (Math.random() * 2 + 1) * 0.25);
+        n.setDuration(n.getRhythmValue() * 0.9);
+        return n;*/
+
+
+        n.setPitch(whistlePitchArray[arrayIndex]);
+        n.setRhythmValue(whistleRhythmArray[arrayIndex]);
+        n.setDynamic(dynoPosition);
+        n.setDuration(n.getRhythmValue());
+
+
+        /*if(arrayIndex % 2 == 0)
+        {
+            n.setDynamic(dynoPosition);
+        }
+        else
+        {
+            setDynoValue(0);
+            //this.dynoPosition = 0;
+        }*/
+        System.out.println("ArrayIndex is: " + arrayIndex);
+        //n.setDynamic(dynoPosition);
+
+        arrayIndex++;
+        if(arrayIndex == whistleRhythmArray.length)
+        {
+            arrayIndex = 0;
+        }
+        return n;
+    }
+
+    /**
+     * Allow other classes to set the notes pan value
+     */
+    public void setDynoValue(int p) {
+        this.dynoPosition = p;
+
+    }
+
+    // added for control change
+    public synchronized void externalAction(Object obj, int actionNumber) {
+        if (actionNumber == 0) {
+            JSlider slider = (JSlider) obj;
+            double filterCutoff = (double) (slider.getValue() * 100);
+            for (int i = 0; i < inst.length; i++) {
+                double[] values = {filterCutoff};
+                inst[i].setController(values);
+            }
+        }
+    }
+}
---
+++ Forte/java/src/edu/virginia/Music/SilenceTest.java
@@ -0,0 +1,116 @@
+package edu.virginia.Music;
+
+import jm.JMC;
+import jm.audio.Instrument;
+import jm.audio.RTMixer;
+import jm.music.rt.RTLine;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SilenceTest implements JMC, ChangeListener, ActionListener {
+    private JSlider dyno;
+    private RTMixer mixer;
+    private JLabel val;
+    //private GBassLine bass;
+
+    private ForteSong trumpet;
+    private ForteSong2 bass;
+    private ForteSong3 whistle;
+
+    private JButton goBtn;
+    private RTLine[] lineArray = new RTLine[3];
+
+    public static void main(String[] args) {
+        new SilenceTest();
+    }
+
+    public SilenceTest() {
+        int sampleRate = 44100;
+        int channels = 2;
+
+        Instrument[] instArray = new Instrument[1];
+        for(int i=0;i<instArray.length;i++){
+            instArray[i] = new SquareLPFInstRT2(sampleRate, 1000, channels);
+        }
+
+        Instrument[] instArray2 = new Instrument[1];
+        for(int i=0;i<instArray2.length;i++){
+            instArray2[i] = new SquareLPFInstRT2(sampleRate, 1000, channels);
+        }
+
+        Instrument[] instArray3 = new Instrument[1];
+        for(int i=0;i<instArray3.length;i++){
+            instArray3[i] = new SquareLPFInstRT2(sampleRate, 1000, channels);
+        }
+
+        trumpet = new ForteSong(instArray);
+        trumpet.setTempo(180);
+        lineArray[0] = trumpet;
+
+        bass = new ForteSong2(instArray2);
+        bass.setTempo(180);
+        lineArray[1] = bass;
+
+        whistle = new ForteSong3(instArray3);
+        whistle.setTempo(180);
+        lineArray[2] = whistle;
+        //mixer = new RTMixer(lineArray);
+
+        // show slider panel
+        makeGUI();
+    }
+
+    private void makeGUI() {
+        JFrame f = new JFrame("Title");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1000, 1000);
+        JPanel panel = new JPanel(new BorderLayout());
+        f.getContentPane().add(panel);
+
+
+        //my dynamic slider
+        dyno = new JSlider(1, 0, 120, 50);
+        dyno.setEnabled(false);
+        dyno.addChangeListener(this);
+
+        panel.add(dyno, "West");
+
+        // filter value display
+        val = new JLabel("1000");
+        panel.add(val, "East");
+
+        goBtn = new JButton("Start");
+        goBtn.addActionListener(this);
+        panel.add(goBtn, "North");
+
+        //
+        f.pack();
+        f.setVisible(true);
+    }
+
+    public void stateChanged(ChangeEvent e){
+        // my dynamic slider moved
+        if (e.getSource() == dyno) {
+            //val.setText("" + (dyno.getValue() * 100));
+            bass.setDynoValue(dyno.getValue()); //does this need a coefficient?
+            //mixer.actionLines(dyno, 2);
+        }
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == goBtn) {
+            mixer = new RTMixer(lineArray);
+            mixer.begin();
+            dyno.setEnabled(true);
+            goBtn.setEnabled(false);
+        }
+    }
+
+
+}
